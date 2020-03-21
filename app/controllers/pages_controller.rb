9d745b9abc2a949e06f21f140867f5664bcc9d23
camille villard <hello@camillevillard.com>
2020-03-21 18:30:42 -0400

    controllers to handle pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def styles
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get styles" do
+    get pages_styles_url
+    assert_response :success
+  end
+
+end
