97f8bd156531b9e0fd470f400fb0bea7843d555a
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-03-22 19:52:45 -0400

    demand controlle rupdate

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,5 +1,16 @@
 class DemandsController < ApplicationController
+  before_action :find_demand, only: %i[show]
+  
   def index
     @demands = Demand.all
   end
+
+  def show
+  end
+
+  private
+
+    def find_demand
+      @demand = Demand.find(params[:id])
+    end
 end
