2520d0e7daa98164fd0f1bfdd14c6f06b1b9ea41
camille villard <hello@camillevillard.com>
2020-03-21 20:47:11 -0400

    ui kit - almost everything but not all

---
+++ app/javascript/packs/add-input.js
@@ -0,0 +1,16 @@
+console.log("prout");
+
+const addItemToggle = document.querySelector("#add-item-toggle");
+const addItemInput = document.querySelector("#add-item-input");
+const addItemText = document.querySelector("#add-item-text");
+
+addItemToggle.addEventListener("click", () => {
+  replaceToggleText();
+  addItemToggle.classList.toggle("open");
+  addItemInput.classList.toggle("hide");
+  addItemText.classList.toggle("hide");
+});
+
+const replaceToggleText = () => {
+  addItemToggle.innerHTML = "<<";
+};
