c2af9f033e90934f5d733aec764e4e5639277ad6
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-03-21 20:03:42 -0400

    need controler

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,2 +1,43 @@
 class NeedsController < ApplicationController
+  before_action :set_need, only: %i[show update destroy]
+  
+  def index
+    @needs = Need.all
+  end
+
+  def show
+    @list = List.new
+  end
+
+  def create
+    @need = Need.new(need_params)
+    @list = List.new
+    @need.list = @lists
+    if @need.save
+      redirect_to needs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @need.update(need_params)
+    redirect_to need_path(@need)
+  end
+
+  def destroy
+    @need.destroy
+    redirect_to needs_path
+  end
+
+  private
+
+    def set_need
+      @need = Need.find(params[:id])
+    end
+
+    def need_params
+      params.require(:need).permit!
+    end
+  
 end
