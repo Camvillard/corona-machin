12732746aa0fa4df2f2949af3bf46d6074e2c9b5
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-03-21 20:24:39 -0400

    need controller

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,5 +1,5 @@
 class NeedsController < ApplicationController
-  before_action :set_need, only: %i[show update destroy]
+  before_action :set_need, only: %i[show update destroy create_item]
   
   def index
     @needs = Need.all
@@ -11,7 +11,7 @@ class NeedsController < ApplicationController
 
   def create
     @need = Need.new(need_params)
-    @list = List.new
+    @list = List.new(list_params)
     @need.list = @lists
     if @need.save
       redirect_to needs_path
@@ -20,12 +20,17 @@ class NeedsController < ApplicationController
     end
   end
 
+  def create_item
+    @list = @need.list
+    @item = Item.new(item_params)
+  end
+
   def update
     @need.update(need_params)
     redirect_to need_path(@need)
   end
 
-  def destroy
+  def delete
     @need.destroy
     redirect_to needs_path
   end
@@ -38,6 +43,14 @@ class NeedsController < ApplicationController
 
     def need_params
       params.require(:need).permit!
+    end
+
+    def list_params
+      params.require(:list).permit!
+    end
+
+    def item_params
+      params.require(:item).permit!
     end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get '/choices', to: "pages#choices"
 
   resources :need do
-    resources :list, only: [:show, :edit, :update, :delete]
+    resources :list, only: [:show, :update] do
+      resources :item, only: [:create]
+    end
   end
+
+  resources :item, only: [:update, :delete]
 end
