521c354937f26affe56ee2a0e8836d3aa8e95fb4
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-03-21 23:10:06 -0400

    list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,19 @@
 class ListsController < ApplicationController
+  before_action :set_item
+
+
+  def update
+  
+  end
+
+  def delete
+    @item.destroy
+  end
+
+  private
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
 end
