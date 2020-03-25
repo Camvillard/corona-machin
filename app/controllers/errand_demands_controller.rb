7824c7bc271dfb059e12341e424c9327218bf6a9
camille villard <hello@camillevillard.com>
2020-03-25 16:31:17 -0400

    js for new errand

--- app/controllers/errand_demands_controller.rb
+++ app/controllers/errand_demands_controller.rb
@@ -3,9 +3,22 @@ class ErrandDemandsController < ApplicationController
     @errand = ErrandDemand.new
     @need = Demand.new
     @list = List.new
+    @item = Item.new
   end
 
   def create
+    @errand = ErrandDemand.create!(
+      address: params[:address]
+    )
+    @need = Demand.new(
+      title: params["title"],
+      name: params["name"],
+      phone: params["phone"],
+      email: params["email"],
+      status: "pending",
+      message: params["message"],
+      need: @errand,
+    )
     raise
   end
 
--- app/javascript/packs/new-errand.js
+++ app/javascript/packs/new-errand.js
@@ -1,9 +1,13 @@
-console.log("prout");
-
 const nameInput = document.querySelector("#name");
 const hiddenNameInput = document.querySelector("#hidden-name");
 
-nameInput.addEventListener("blur", e => {
-  console.log(e.target.value);
-  hiddenNameInput.value = e.target.value;
+const inputs = document.querySelectorAll(".for-hidden-input");
+const hiddenInputs = Array.from(document.querySelectorAll(".hidden-input"));
+
+inputs.forEach(input => {
+  input.addEventListener("blur", event => {
+    const { value, name } = event.target;
+    const matchingInput = hiddenInputs.find(input => input.id.includes(name));
+    matchingInput.value = value;
+  });
 });
