ef23d4d990801854b0170d7d482cdf75a693f77e
camille villard <hello@camillevillard.com>
2020-03-29 17:57:12 -0400

    map + style

--- Gemfile
+++ Gemfile
@@ -15,7 +15,8 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
-gem 'webpacker', '~> 4.0'
+# gem 'webpacker', '~> 4.0'
+gem 'webpacker', '~> 3.5.5'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,24 @@
+import mapboxgl from "mapbox-gl";
+// handle the display or no the map
 const mapToggle = document.querySelector("#map-toggle");
 const mapContainer = document.querySelector(".show-map-container");
+const mapElement = document.querySelector("#map");
 
 mapToggle.addEventListener("click", () => {
   mapContainer.classList.toggle("open");
 });
+
+const initMapbox = () => {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiY2FtdmlsbGFyZCIsImEiOiJjazhkamkyY2QwdjBuM210N2txbHk2YmZhIn0.KRmSud_67BKoDUanEAWIMw";
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [-77.04, 38.907],
+    zoom: 11.15
+  });
+};
+
+if (mapElement) {
+  initMapbox();
+}
