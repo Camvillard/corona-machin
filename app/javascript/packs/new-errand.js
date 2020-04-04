f45b0d94e19376b9d29d3c2bd284abd5cb235235
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-04-04 13:52:14 -0400

    all new demand validation added

--- app/javascript/packs/new-errand.js
+++ app/javascript/packs/new-errand.js
@@ -3,9 +3,6 @@ const addItemInput = document.querySelector(".add-item-input");
 const qtyItemInput = document.querySelector(".quantity-item-input");
 const submitItemButton = document.querySelector("#submit-item-btn");
 const listItemsContainer = document.querySelector(".list-items-container");
-const submitButton = document.querySelector("#submit-button");
-const validatedInput = document.querySelectorAll(".validation-form");
-
 
 submitItemButton.addEventListener("click", event => {
   addItemToList();
@@ -30,26 +27,4 @@ const displayNewItems = () => {
 const clearInputs = () => {
   addItemInput.value = "";
   qtyItemInput.value = "";
-};
-
-submitButton.addEventListener("click", event => {
-  console.log("validation input", validatedInput)
-  validatedInput.forEach((inputDiv) => {
-    displayErrorMessage(inputDiv);
-  })
-})
-
-const displayErrorMessage = (inputDiv) => {
-
-  const input = inputDiv.querySelector("input")
-  const select = inputDiv.querySelector("select")
-  const errorMessage = inputDiv.querySelector(".error-message");
-
-  if (input) {
-    errorMessage.innerHTML = !input.value ? "ce champs ne peut pas être vide" : ""
-  };
-
-  if (select) {
-    errorMessage.innerHTML = !select.value ? "ce champs ne peut pas être vide" : ""
-  }
 };
